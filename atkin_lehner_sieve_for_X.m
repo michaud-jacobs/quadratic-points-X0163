a17d8ee0f7aa7c1d7e08627462a7995d91ea6f07
Philippe Michaud-Jacobs <102212055+michaud-jacobs@users.noreply.github.com>
2023-10-10 16:21:57 +0100

    Include parameters for running sieve

--- atkin_lehner_sieve_for_X.m
+++ atkin_lehner_sieve_for_X.m
@@ -1,3 +1,8 @@
 load "AL_sieve_auxiliary.m";
 
+P_CM_seq := [0, 0,0,0,0,0,54/11,-51/11,35/11,-2,23/11,-9/11,1];
+primes_to_ignore := {p : p in PrimesInInterval(7,30)};
+AL_sieve(163 : extra_rational_points := {P_CM_seq}, badPrimes := primes_to_ignore);
+
+
 
