d285fe4c78caf53117bc8403ada1b3fe11b96eeb
Philippe Michaud-Jacobs <102212055+michaud-jacobs@users.noreply.github.com>
2023-10-10 13:21:54 +0100

    Make file loadable without running code

--- Atkin_Lehner_Sieve/pullbacks.m
+++ Atkin_Lehner_Sieve/pullbacks.m
@@ -126,43 +126,8 @@ end function;
 
 ////////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////
-
-// For each curve for which we apply the AL sieve, we can list the pullback points we obtain.
-// We may also list the j-invariant of each point, and its CM discriminant if appropriate
-
-for N in [58, 68, 74, 76, 80, 85, 97, 98, 100, 103, 107, 109, 113, 121, 127] do
-    print "N =", N;
-    al_seq := [ [m] : m in Divisors(N) | GCD(m,N div m) eq 1 and m gt 1]; // all AL involutions
-    X, _, pairs := eqs_quos(N, al_seq);
-    bound := 10000;
-    pullbacks := pullback_points(X,pairs,N, bound);
-    print "Number of pairs of quadratic points found as pullbacks =", #pullbacks;
-    print "++++++++++++++++++";
-    j := jmap(X,N);
-    for tup in pullbacks do
-        K<T> := tup[2];
-        P := X(K) ! Eltseq(tup[1][1]);
-        T2 := -Coefficient(DefiningPolynomial(Ring(Parent(P))),0); // T^2 = this
-        jP := j(P)[1];
-        if jP eq 1 and j(P)[2] eq 0 then 
-            print "P coordinates:", P, "where T^2 =", T2, "and the point is a cusp";
-            continue;
-        end if;
-        tf, D := HasComplexMultiplication(EllipticCurveWithjInvariant(jP));
-        if tf then
-            print "P coordinates:", P, "where T^2 =", T2, "and j-invariant =", jP, "and CM by", D;
-        else
-            print "P coordinates:", P, "where T^2 =", T2, "and j-invariant =", jP, "and no CM";
-        end if;
-    end for;
-    print "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++";
-end for;
-
-
-////////////////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////////
 /*
-// Further Examples //
+// Examples //
 
 // Example 1
 // We consider the curve X_0(34).
