b6c5a2c005446c2afc850e8d7f151d6ac248b039
Philippe Michaud-Jacobs <102212055+michaud-jacobs@users.noreply.github.com>
2023-10-12 22:15:58 +0100

    add a runtime

--- display_model.m
+++ display_model.m
@@ -1,7 +1,7 @@
 // We display a model for the curve X in P^12 together with the matrix w on this model
 // The output of this file is included at the end of the file
 
-load "Atkin-Lehner_sieve/models_and_maps.m";
+load "Atkin-Lehner_sieve/models_and_maps.m"; // Runtime: ~ 1 minute
 X, ws := eqs_quos(163,[]);
 print "Model for X in P^12 is given by: ", X;
 print "The Atkin-Lehner involution w acts via: ", Matrix(ws[1]);
