36010bcb8a94216664c3293b9236ddb37bfd2579
Philippe Michaud-Jacobs <102212055+michaud-jacobs@users.noreply.github.com>
2023-10-12 23:00:32 +0100

    Update fixed_points_and_loneliness_additional_checks.m
    remove extra space

--- fixed_points_and_loneliness_additional_checks.m
+++ fixed_points_and_loneliness_additional_checks.m
@@ -101,9 +101,3 @@ c_0_plus_P_CM := Divisor(c_0) + Divisor(P_CM); // Runtime: ~ 1 hour
 for p in PrimesInInterval(5, 37) do // Runtime: ~ 3 minutes
     assert IsLonely(c_0_plus_P_CM, p, X, Mw, g_quo);
 end for;
-    
-
-
-
-
-
